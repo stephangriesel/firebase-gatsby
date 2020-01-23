828894527839395ce88eb32ca806aa39c78b497d
Stephan <sgriesel@gmail.com>
2020-01-23 18:41:44 +0100

    update meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Book Club`,
+    description: `Discuss your latest read`,
+    author: `@stevegriesel`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
-    // {
-
-    // },
     {
       resolve: 'gatsby-firesource',
       options: {
